4070c727834afe929d3527eb91a3c601d630d2c6
Matthew <ashlander@live.com>
2021-01-21 02:35:13 -0600

    Bubble chart finished and meta started

--- static/js/app.js
+++ static/js/app.js
@@ -9,24 +9,32 @@ d3.json('data/samples.json').then(response => {
 	var samples = response.samples;
 	
 	// variables
-	var otuIDs = [samples[0].otu_ids.slice(0, 10).reverse()];
-	var otuLabels = [samples[0].otu_labels.slice(0, 10)];
-	var quantity = [samples[0].sample_values.slice(0, 10).reverse()];
+	var otuIDs = [];
+	var otuIDstr = [];
+	var otuLabels = [];
+	var quantity = [];
 
 	function onChange() {
 		// get value selected from dropdown
 		value = d3.select('#selDataset').property('value');
 
+		// create new variables for charts
 		otuIDs = samples[value].otu_ids.slice(0, 10).reverse();
-		otuIDs = otuIDs.map(otu => 'OTU ' + otu);
+		otuIDstr = otuIDs.map(otu => 'OTU ' + otu);
 		otuLabels = samples[value].otu_labels.slice(0, 10);
 		quantity = samples[value].sample_values.slice(0, 10).reverse();
 		
 		print(otuIDs);
 
+		// refresh bar chart
 		Plotly.restyle('bar', 'x', [quantity]);
-		Plotly.restyle('bar', 'y', [otuIDs]);
+		Plotly.restyle('bar', 'y', [otuIDstr]);
 		Plotly.restyle('bar', 'text', [otuLabels]);
+		// refresh bubble chart
+		Plotly.restyle('bubble', 'x', [otuIDs]);
+		Plotly.restyle('bubble', 'y', [quantity]);
+		Plotly.restyle('bubble', 'marker.color', [otuIDs]);
+		Plotly.restyle('bubble', 'marker.size', [quantity]);
 	};
 
 	// bar chart
@@ -38,19 +46,47 @@ d3.json('data/samples.json').then(response => {
 			orientation: 'h',
 			text: otuLabels
 		};
-		print(quantity);
-		print(otuIDs);
 
 		var layout = {
 			title: 'Top 10 OTUs',
-			yaxis: {title: 'OTU',
-					tickmode: 'linear',
+			yaxis: {tickmode: 'linear',
 					type: 'category'},
 			xaxis: {title: 'Quantity of OTU'}
 		};
 		var data = [trace];
 		
 		Plotly.plot('bar', data, layout);
+	};
+
+	// bubble chart
+	function buildBubble() {
+		var trace = {
+			x: [],
+			y: [],
+			type: 'scatter',
+			mode: 'markers',
+			marker: {
+				color: '',
+				size: 0				
+			},
+			text: otuLabels
+		};
+
+		var data = [trace];
+
+		var layout = {
+			title: 'Top 10 OTUs'
+		};
+
+		Plotly.plot('bubble', data, layout);
+	};
+
+	// populate meta data
+	function buildMeta() {
+		//get table
+		var table = d3.select('sample-metadata');
+
+		
 	};
 
 	// fill dropdown options
@@ -64,6 +100,9 @@ d3.json('data/samples.json').then(response => {
 	// initilization function to build the plots
 	function init() {
 		buildBar();
+		buildBubble();
+		buildMeta();
 	};
+
 	init();
 });
